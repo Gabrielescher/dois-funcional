3335fc106256353d647ce81e62de41561bb89558
Gabrielescher <127734429+Gabrielescher@users.noreply.github.com>
2024-06-05 09:19:19 -0300

    Add files via upload

---
+++ dois/script.js
@@ -0,0 +1,12 @@
+function corazon () {
+    alert("Esse algoritmo ira calcular quantas vezes o seu S2 bateu desde o ano de nascimento");
+    let n = prompt ("digite o ano do seu nascimento")
+    let a = 2024-n
+    let m = a*12
+    let d = m*30
+    let h = d*24
+    let min = h*60
+    let b = min*70
+    alert ("O seu S2 bateu até 2024  " +b)
+}
+corazon();
